c2fab97666413c18c15c3127b3fc2ba945010715
Mackenzie Gray <mdkulen@gmail.com>
2022-07-05 17:45:07 +1000

    added script.js variables

--- Assets/script.js
+++ Assets/script.js
@@ -8,15 +8,19 @@ var currentDate = dayjs('').format('DD/MM/YYYY')
 var city = document.getElementById('#cityInput');
 
 // variable for search button and clear history button
-var searchButton = document.getElementById('#searchButton');
-var clearHistoryButton = document.getElementById('#clearHistoryButton');
+var searchButton = $('#searchButton');
+var clearHistoryButton = $('#clearHistoryButton');
 
 // variable for city spotlight display 
+var chosenCity = $('#chosenCity');
 
 //variable for 5 day forecast 
+var fiveDayForecast = $('#forecast');
 
 // event listeners for search button and clear history button
-
+searchButton.on('click', function () {
+    alert('Hello World');
+});
 
 
 // function for API call 
@@ -30,3 +34,15 @@ var clearHistoryButton = document.getElementById('#clearHistoryButton');
 // function to append local storage to page 
 
 // function to clear local storage
+
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
