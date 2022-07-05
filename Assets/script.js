0d50bab942b45446f1645ac0c1f9a6ba95b7b514
Mackenzie Gray <mdkulen@gmail.com>
2022-07-05 17:09:21 +1000

    added variables to script.js

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+// global variables
+var APIKey = '94f3fb3dbc8fcc33a80f69c4935cdade';
+
+// variable for current date with day/js
+var currentDate = dayjs('').format('DD/MM/YYYY')
+
+// query selectors for user input fields
+var city = document.getElementById('#cityInput');
+
+// variable for search button and clear history button
+var searchButton = document.getElementById('#searchButton');
+var clearHistoryButton = document.getElementById('#clearHistoryButton');
+
+// variable for city spotlight display 
+
+//variable for 5 day forecast 
+
+// event listeners for search button and clear history button
+
+
+
+// function for API call 
+
+// function to append data results into display fields
+
+// functions for uv index colours 
+
+//function for local storage 
+
+// function to append local storage to page 
+
+// function to clear local storage
